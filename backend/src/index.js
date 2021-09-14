28fec54d4655c99e6aa63190574f9327a27e8ff1
wilgnerl <wilgnerl@al.insper.edu.br>
2021-09-14 20:52:41 -0300

    set port heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('express-async-errors');
-
+require('dotenv').config();
 const routes = require('./routes');
 
 const app = express();
@@ -15,4 +15,4 @@ app.use((error, request, response, next) => {
   response.sendStatus(500);
 });
 
-app.listen(3000, () => console.log('Server start in http://localhost:3000/'));
+app.listen(process.env.PORT || 3000, () => console.log('Server start in http://localhost:3000/'));
