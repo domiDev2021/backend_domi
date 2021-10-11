e6bd43d3f3f7db9fa1d96531430149ed9fb79002
wilgnerl <wilgnerl@al.insper.edu.br>
2021-10-11 16:14:10 -0300

    Revert "cookies"
    This reverts commit 6cf698d05e843a7beda4b3ad941776e071524a8d.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 require('express-async-errors');
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-const corsOptions = {
-  exposedHeaders: 'authorization-token',
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 app.use((error, request, response, next) => {
